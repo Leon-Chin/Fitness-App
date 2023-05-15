e9bc7b3c745c0cafe071749ff817fa4efa921646
qinlh <2394416759@qq.com>
2023-05-16 04:21:23 +0800

    feat: add the layout framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.less';
+import Sidebar from './components/sidebar'
 
 function App() {
   return (
     <div className="App">
-      你好
+      <div className='sidebar'>
+
+      </div>
+      <div className='contentBox'>
+        <div className='contendBox-left'>
+          <div className='sayHello'>
+            Hello Leon <br />
+            Welcome to the fitness app
+          </div>
+          <div className='contendBox-left-subTop'>
+            <div className='contacts'></div>
+            <div className='statistic'></div>
+          </div>
+          <div className='contendBox-left-subBottom'>
+            <div className='model1'></div>
+            <div className='trend'></div>
+          </div>
+        </div>
+        <div className='contendBox-right'></div>
+      </div>
     </div>
   );
 }
